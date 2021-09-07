80e4d2fae0800cf01f4ad6fb0c142e7eac43e4a9
rohitbankar21 <89958852+rohitbankar21@users.noreply.github.com>
2021-09-07 16:20:58 +0530

    Add files via upload

---
+++ Area.java
@@ -0,0 +1,13 @@
+class Area
+{
+	public static void main(String[] args)
+	{
+		aoc(3);
+	}
+	static void aoc(double rad)
+	{
+		float pi=3.14f;
+		double area=pi*rad*rad;
+		System.out.println("AOC IS="+area);
+	}
+}
