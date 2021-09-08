f85c4bc1b6f8128218bb44054f44bcc0a0b7e732
rohitbankar21 <89958852+rohitbankar21@users.noreply.github.com>
2021-09-08 22:14:47 +0530

    Add files via upload

---
+++ Number.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+class Number
+{
+	public static void main(String[] args)
+	{
+		Scanner sc1=new Scanner(System.in);
+
+		System.out.println("PLEASE ENTER FIRST NUMBER");
+		double no1=sc1.nextDouble();
+		System.out.println("PLEASE ENTER SECOND NUMBER");
+		double no2=sc1.nextDouble();
+
+		if(no1>no2)
+		{
+			System.out.println("Display greatest no");
+		}
+		else
+		{
+			System.out.println("Display greatest no");
+		}
+
+	}
+
+}
